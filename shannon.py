fb233fb4ddecbada8203ce740af1409501851906
abrelle <46755278+abrelle@users.noreply.github.com>
2020-12-13 23:17:15 +0200

    bug fixed

--- shannon.py
+++ shannon.py
@@ -1,6 +1,3 @@
-
-#viskas bus gerai Zivile, susigaudysi
-
 def read_input_from_file(filename: list) -> str:
     try:
         with open(filename, mode='r') as file:  # reading characters from file
@@ -16,15 +13,28 @@ def probability_table(word: list) -> dict:
     length = len(word)
 
     for letter in differentValues:
-        p = word.count(letter)/length
-        probTable.update({letter: p})   # e.g. {'a':0.25}
+        p = word.count(letter) / length
+        probTable.update({letter: p})  # e.g. {'a':0.25}
 
-    sorted_probTable = sorted(probTable.items(), key=lambda x: x[1], reverse=True)
+    #PATAISYTA DALIS. Buvo klaida nes grazindavo sarasa, o ne zodyna
+    sorted_tuples = sorted(probTable.items(), key=lambda x: x[1], reverse=True) #grazina surikiuota poru SARASA
+    sorted_probTable = {k: v for k, v in sorted_tuples}  #is poru SARASO i zodyna
 
     return sorted_probTable
+
+
+def to_binary_table(probTable: dict) -> dict:
+    sum = 0
+    binaryTable = {}
+    for key, value in probTable.items():
+        #neveikia
+        binaryTable.update({key: format(sum, "08b")}) #format(<the_integer>, "<0><width_of_string><format_specifier>")
+        sum += value
+    return binaryTable
 
 
 if __name__ == '__main__':
     word = read_input_from_file("test.txt")
     table = probability_table(word)
-    print(table)
+    bTable = to_binary_table(table)
+    print(bTable)

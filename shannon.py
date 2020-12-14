5f3b41d51f773eb8cd611080f136b2b368f5d445
abrelle <46755278+abrelle@users.noreply.github.com>
2020-12-14 18:39:42 +0200

    added functionality

--- shannon.py
+++ shannon.py
@@ -1,3 +1,9 @@
+import math
+from bitarray import bitarray
+
+Base = 2
+
+
 def read_input_from_file(filename: list) -> str:
     try:
         with open(filename, mode='r') as file:  # reading characters from file
@@ -16,19 +22,37 @@ def probability_table(word: list) -> dict:
         p = word.count(letter) / length
         probTable.update({letter: p})  # e.g. {'a':0.25}
 
-    #PATAISYTA DALIS. Buvo klaida nes grazindavo sarasa, o ne zodyna
-    sorted_tuples = sorted(probTable.items(), key=lambda x: x[1], reverse=True) #grazina surikiuota poru SARASA
-    sorted_probTable = {k: v for k, v in sorted_tuples}  #is poru SARASO i zodyna
+    # PATAISYTA DALIS. Buvo klaida nes grazindavo sarasa, o ne zodyna
+    sorted_tuples = sorted(probTable.items(), key=lambda x: x[1], reverse=True)  # grazina surikiuota poru SARASA
+    sorted_probTable = {k: v for k, v in sorted_tuples}  # is poru SARASO i zodyna
 
     return sorted_probTable
+
+
+def length_of_bin_from_prob(probability: float) -> int:
+    log_len = math.log(probability, Base)
+    return math.ceil(-log_len)
+
+
+def float_to_bin(value: float, length: int) -> bitarray:
+    bits = bitarray()
+    newNum = value
+    for i in range(0, length):
+        newNum *= 2
+        if newNum > 1:
+            bits.append(True)
+            newNum -= 1
+        else:
+            bits.append(False)
+    return bits
 
 
 def to_binary_table(probTable: dict) -> dict:
     sum = 0
     binaryTable = {}
     for key, value in probTable.items():
-        #neveikia
-        binaryTable.update({key: format(sum, "08b")}) #format(<the_integer>, "<0><width_of_string><format_specifier>")
+        # neveikia, reikia papildomo algoritmo
+        binaryTable.update({key: format(sum, "08b")})  # format(<the_integer>, "<0><width_of_string><format_specifier>")
         sum += value
     return binaryTable
 
